44f94549bc864ec260706014753294e859af6d0f
Shoichi Sakane <github-shoichi@tanu.org>
2018-06-04 10:12:43 +0900

    supported py2 and py3

--- app_parser/__init__.py
+++ app_parser/__init__.py
@@ -1,2 +1,8 @@
-import parser_gs01
-import parser_hga01
+try:
+    from . import parser_gs01
+    from . import parser_hga01
+    from . import parser_hoku01
+except:
+    import parser_gs01
+    import parser_hga01
+    import parser_hoku01
