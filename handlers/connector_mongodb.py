3473b0f0ed413d053f41fed904a9656512628f9e
Shoichi Sakane <github-shoichi@tanu.org>
2018-07-22 18:06:40 +0900

    - added a handler using sqlite3. - added templates for mongodb and sqlite3 to ease adding custom   handlers. - documentation.

---
+++ handlers/connector_mongodb.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+from app_util import iso8601_to_ms
+from parser_template import parser_template
+
+class connector_mongodb(parser_template):
+
+    '''
+    you can use the following code to submit data into the default
+    mongodb.
+
+        from connector_mongodb import connector_mongodb
+
+        class parser(connector_mongodb):
+            pass
+
+        p = parser(logger=self.logger, debug_level=self.debug_level,
+                   db_uri="your_mongodb_rest_api")
+        p.submit(kv_data)
+
+    '''
+    def db_init(self, **kwargs):
+        '''
+        db_uri must be passed.
+        '''
+        db_uri = kwargs.get("db_uri")
+        if db_uri is None:
+            raise ValueError("db_uri is required in the handler.")
+        self.db_uri = db_uri
+        return True
+
+    def db_submit(self, kv_data, **kwargs):
+        '''
+        - submit json data into the MongoDB via REST API.
+        - assuming that all keys are exist in the json data.
+          because it assumes that proc_tp_uplink() has been called already.
+        - assuming that the "Time" key has the ISO8601 string value like below:
+            "2017-03-24T06:53:32.502+01:00".
+        - if the string looks naive, it will convert into the timezone
+          when you specified at calling db_init().
+        '''
+        kv_data["Time"] = {"$date": iso8601_to_ms(kv_data["Time"])}
+        # submit.
+        ret = requests.post(self.db_uri, data=json.dumps(kv_data))
+        if ret.ok:
+            ret_value = ret.json()
+            if ret_value["ok"] is True:
+                if self.debug_level > 0:
+                    self.logger.debug("Submitting data into MongoDB succeeded.")
+                    return True
+            else:
+                self.logger.error("Response from MongoDB: {}".format(ret_value))
+                return False
+        else:
+            self.logger.error("Submitting data into MongoDB failed. {} {} {}"
+                .format(ret.status_code, ret.reason, ret.text))
+            return False
+
---
+++ handlers/connector_sqlite3.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import sqlite3
+import re
+from parser_template import parser_template
+from app_util import iso8601_to_fixed_ts
+
+class connector_sqlite3(parser_template):
+
+    '''
+    you can use the following code to submit data into the default
+    sqlite3.
+
+        from connector_sqlite3 import connector_sqlite3
+
+        class parser(connector_sqlite3):
+            pass
+
+        p = parser(logger=self.logger, debug_level=self.debug_level,
+                   db_name="your_database_name")
+        p.submit(kv_data)
+    '''
+    def db_init(self, **kwargs):
+        '''
+        this method will call self.create_db() even if it is not needed.
+        '''
+        db_name = kwargs.get("db_name")
+        if db_name is None:
+            raise ValueError("db_name is required in the handler.")
+        self.db_name = db_name
+        self.cur = None
+        if self.db_name is not None:
+            self.con = sqlite3.connect(self.db_name)
+            self.cur = self.con.cursor()
+        if self.cur is None:
+            raise ValueError("cursor has not been opened.")
+        #
+        return self.create_db(**kwargs)
+
+    def db_submit(self, kv_data, **kwargs):
+        '''
+        this method will call self.make_insert_string().
+        the value of make_insert_string method:
+            None: ignore this data.
+            (string): insert SQL
+        '''
+        if self.cur is None:
+            raise ValueError("{} is not connected.".format(self.db_name))
+        return self.insert_db(kv_data, **kwargs)
+
+    def fix_ts(self, ts):
+        return iso8601_to_fixed_ts(ts, self.tz)
